fdb58757f5204a2039389fa9ba6376e7e04fe851
Shubhamsahar <kumarshubham765484@gmail.com>
2025-07-30 17:03:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const makeChange = (c) => {
   // your name here
+	 c = parseInt(c);
+    let change = {
+        q: Math.floor(c / 25),
+        d: Math.floor((c % 25) / 10),
+        n: Math.floor(((c % 25) % 10) / 5),
+        p: ((c % 25) % 10) % 5
+    };
+    return change;
+
 };
 
 // Do not the change the code below
